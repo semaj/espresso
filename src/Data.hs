a27a89ede55e242de403d18423f8d731d484475a
James Larisch <jamesfordummies@gmail.com>
2016-03-27 22:43:09 -0400

    Add Data (Filter, FilterDiff) with tests
    We need functions for combining filter diffs and
    serializing filters into json.

---
+++ src/Data.hs
@@ -0,0 +1,25 @@
+module Data where
+
+import           Data.Aeson
+import           Data.List
+import qualified Data.Map.Strict as M
+import qualified GHC.Exts as E
+
+data Filter = Filter { filterID :: Int
+                     , filterData :: [Int]
+                     }
+
+-- Index of bloom filter bit array (as string) => new value
+type FilterDiff = M.Map String Int
+
+coalesce :: [FilterDiff] -> FilterDiff
+coalesce = foldl' M.union M.empty
+
+filterDiffJson :: [FilterDiff] -> Value
+filterDiffJson fds = Object $ E.fromList [("filter_diff", toJSON $ coalesce fds)]
+
+instance ToJSON Filter where
+  toJSON Filter{..} = object [
+    "filter_id" .= filterID,
+    "filter_data" .= filterData
+    ]
---
+++ test/DataSpec.hs
@@ -0,0 +1,21 @@
+module DataSpec where
+
+import           Data
+import           Data.Aeson
+import qualified Data.Map.Strict as M
+import           Test.Hspec
+import qualified Test.Hspec.Wai.JSON as J
+
+spec :: Spec
+spec = do
+  let fd1 = M.fromList [("1", 1), ("2", 2)]
+      fd2 = M.fromList [("1", 3), ("3", 3)]
+      combined = M.fromList [("1", 1), ("2", 2), ("3", 3)]
+
+  describe "coalesce" $ do
+    it "should favor earlier filter diffs" $ do
+      coalesce [fd1, fd2] `shouldBe` combined
+
+  describe "filterDiffJson" $ do
+    it "should encode as json correctly" $ do
+        encode (filterDiffJson [fd1, fd2]) `shouldBe` [J.json| {"filter_diff": {"1": 1, "2": 2,"3": 3 } } |]
